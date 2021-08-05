9e7d38e84cf9d50c8c7fa3f6cf4d10efdb6149f9
Tai <nguyentutai12@gmail.com>
2021-08-05 20:54:52 +0700

    change message

--- src/pages/staff/StaffProfile.js
+++ src/pages/staff/StaffProfile.js
@@ -265,7 +265,7 @@ export default function StaffProfile(props) {
               });
             });
             message.success({
-              content: "Update staff success!",
+              content: "Update staff successfully!",
               key_message,
               duration: 2,
             });
